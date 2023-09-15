c1b62db8d1ef072dd17154feae810351b6c1b128
Daniel Alexander Dam <dani36i9@stud.kea.dk>
2023-09-15 11:09:08 +0200

    round files + setup af Program

--- Football_Standings/Program.cs
+++ Football_Standings/Program.cs
@@ -1,47 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Football_Standings;
-using System.IO;
-using System.Reflection;
-using System;
-using System.IO;
 
-
-
+// load teams and setup
 var (teamsList, teamMapDictionary) = CsvProcessor.LoadTeamsFromCsv(Path.Combine("test", "teams.csv"));
+
 Console.Write("hello");
 
+// display 
 
+// loop(22) for round files 22
 
+// display
 
+// upper lower 10 rounds
 
+// display
 
-
-
-
-
-
-
-// // path to the csv file
-// string directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-// string path = Path.Combine(directory, "test", "teams.csv");
-// string projectRootDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
-
-
-// Console.WriteLine("path");
-// Console.WriteLine(directory);
-// Console.WriteLine(path);
-// Console.WriteLine(projectRootDirectory);
-// Console.WriteLine(Path.Combine(projectRootDirectory, "abc",  "test.csv"));
-// Console.WriteLine(Path.Combine("abc",  "test.csv"));
-
-
-
-// string[] lines = File.ReadAllLines(path);
-// foreach(string line in lines)
-// {
-//     string[] columns = line.Split(',');
-//     foreach (string column in columns) {
-//         Console.WriteLine(column);
-//     }
-// }
