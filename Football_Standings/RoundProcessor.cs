1fde3e1cc1b612ab6a0d16df04d45609380fc088
CodeClod <mika9103@stud.kea.dk>
2023-09-17 15:56:56 +0200

    Work On RoundProcessor.cs

--- Football_Standings/RoundProcessor.cs
+++ Football_Standings/RoundProcessor.cs
@@ -2,12 +2,12 @@ namespace Football_Standings;
 
 public class RoundProcessor
 {
-    public void ProcessRoundFiles(Dictionary<string,Team> teams)
+    public Dictionary<string, Team> ProcessRoundFiles(Dictionary<string, Team> teams)
     {
-        
-        
+
+
         var teamMap = teams;
-        
+
 
         using (var reader = new StreamReader(Path.Combine(_rootDir, filepath)))
         {
@@ -18,53 +18,48 @@ public class RoundProcessor
             {
                 var line = reader.ReadLine();
                 var values = line.Split(',');
-                
-                    foreach (var teamValuePair in teamMap)
+
+                if (teamMap.ContainsKey(values[0]) & teamMap.ContainsKey(values[2]))
+                {
+                    var hometeamGoals = Int16.Parse(values[1]);
+                    var outteamGoals = Int16.Parse(values[3]);
+
+                    // Win/Loss/Draw calculation
+
+                    if (hometeamGoals > outteamGoals)
                     {
-                        if (teamValuePair.Key.Contains(values[0]) & teamValuePair.Key.Contains(values[2]))
-                        {
-                            // Adds Win/Loss/Draw to team based on info from round
-                            if (Int16.Parse(values[1]) > Int16.Parse(values[3]))
-                            {
-                                teamValuePair.Value.Wins += 1;
-                            }
-                            else if (Int16.Parse(values[1]) < Int16.Parse(values[3]))
-                            {
-                                teamValuePair.Value.Losses += 1;
-                            }
-                            else
-                            {
-                                teamValuePair.Value.Draws += 1;
-                            }
-                            
-                            // 
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                        }
-                        
+                        teamMap[values[0]].Wins += 1;
+                        teamMap[values[2]].Losses += 1;
                     }
+                    else if (hometeamGoals < outteamGoals)
+                    {
+                        teamMap[values[0]].Losses += 1;
+                        teamMap[values[2]].Wins += 1;
+                    }
+                    else
+                    {
+                        teamMap[values[0]].Draws += 1;
+                        teamMap[values[2]].Draws += 1;
+                    }
+                    
+                    // Goal for & goal against
+                    
+                    teamMap[values[0]].GoalsFor += hometeamGoals;
+                    teamMap[values[2]].GoalsAgainst += outteamGoals;
 
-            // Assuming the CSV format is: Abbreviation,Full club name,Special ranking
-                var team = new Team(values[0], values[1], values[2]);
+                    teamMap[values[1]].GoalsFor += outteamGoals;
+                    teamMap[values[3]].GoalsAgainst += hometeamGoals;
 
-                teams.Add(team);
-                teamMap[team.Abbreviation] = team;
+                }
+
             }
-        }
-        
-        
-        try
-        {
             
         }
-        catch (Exception e)
-        {
-            
-        }
+
+        return teamMap;
+
     }
 }
+        
+        
+        
