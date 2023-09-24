40d3c65bd21ca38a86d93ff421e078c3e12ab377
Daniel Alexander Dam <dani36i9@stud.kea.dk>
2023-09-24 11:18:04 +0200

    Co-authored-by: CodeClod <CodeClod@users.noreply.github.com>
    setup of main menu for selection of scenario

--- Football_Standings/Program.cs
+++ Football_Standings/Program.cs
@@ -3,16 +3,61 @@
 using Football_Standings;
 using Football_Standings.test;
 
+// ### generate data - COMMENT OUT ###
+// CsvGenerator.Generate22("test");
+// CsvGenerator.Generate10("test");
+
+// main loop for program (for the user to choose a scenario to play out)
+// userinput for choosing a type of test ie. what folder to read with
+
+
+
+
 // load teams and setup
 var (teamsList, teamMapDictionary) = CsvProcessor.LoadTeamsFromCsv(Path.Combine("test", "teams.csv"));
 var league = CsvProcessor.LoadLeagueFromCsv("test");
 
-CsvGenerator.Generate22("test");
-CsvGenerator.Generate10("test");
-RoundProcessor.ProcessLeague(teamMapDictionary, league, "test");
 
 
-// Console.WriteLine(LeagueProcessor.LoadLeagueFromCsv("E:\\Code\\RiderProjects\\FootballProject\\Football_Standings\\CsvFiles\\TestLeagueCsv.csv").LeagueName);
+// folder
+// RoundProcessor.ProcessLeague(teamMapDictionary, league, "test");
+// Console.WriteLine("they did the first");
+// sub folder
+// RoundProcessor.ProcessLeague(teamMapDictionary, league, Path.Combine("test", "playAgainstSelf"));
+// RoundProcessor.ProcessLeague(teamMapDictionary, league, Path.Combine("test", "sameOpponent"));
+
+bool running = true;
+while (running)
+{
+    // string input = """
+    // 1. run complete turnament 32 rounds
+    // 2. run test team playing itself
+    // 3. run test team playing itself
+    // [Enter] to quit
+    // """;
+
+    // Console.WriteLine(input);
+    Console.WriteLine("1. run complete turnament 32 rounds");
+    Console.WriteLine("2. run test team playing itself");
+    Console.WriteLine("Enter to quit");
+    string userInput = Console.ReadLine();
+
+    switch (userInput)
+    {
+        case "1":
+            RoundProcessor.ProcessLeague(teamMapDictionary, league, "test");
+            break;
+        case "2":
+            RoundProcessor.ProcessLeague(teamMapDictionary, league, "test");
+            break;
+
+        default:
+            running = false;
+            break;
+    }
+
+
+}
 
 
 
--- Football_Standings/utility/RoundProcessor.cs
+++ Football_Standings/utility/RoundProcessor.cs
@@ -6,115 +6,14 @@ public static class RoundProcessor
 {
     static string _rootDir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
 
-    static public void ProcessRoundFiles(Dictionary<string, Team> dictTeams, string filepath)
-    {
-        var teamMap = dictTeams;
-
-
-        using (var reader = new StreamReader(Path.Combine(_rootDir, filepath, "round-1.csv")))
-        {
-            // Read to discard the header
-            reader.ReadLine();
-
-            while (!reader.EndOfStream)
-            {
-                //
-                var line = reader.ReadLine();
-                var values = line.Split(',');
-
-
-                if (teamMap.ContainsKey(values[0]) && teamMap.ContainsKey(values[2]))
-                {
-                    var hometeamGoals = Int16.Parse(values[1]);
-                    var outteamGoals = Int16.Parse(values[3]);
-
-                    // Win/Loss/Draw calculation
-
-                    if (hometeamGoals > outteamGoals)
-                    {
-                        teamMap[values[0]].Wins += 1;
-                        teamMap[values[2]].Losses += 1;
-                    }
-                    else if (hometeamGoals < outteamGoals)
-                    {
-                        teamMap[values[0]].Losses += 1;
-                        teamMap[values[2]].Wins += 1;
-                    }
-                    else
-                    {
-                        teamMap[values[0]].Draws += 1;
-                        teamMap[values[2]].Draws += 1;
-                    }
-
-                    // Games for and against for home and away teams
-                    //home
-                    teamMap[values[0]].GoalsFor += hometeamGoals;
-                    teamMap[values[0]].GoalsAgainst += outteamGoals;
-                    //away
-                    teamMap[values[2]].GoalsFor += outteamGoals;
-                    teamMap[values[2]].GoalsAgainst += hometeamGoals;
-
-                    // Games played
-                    teamMap[values[0]].GamesPlayed += 1;
-                    teamMap[values[2]].GamesPlayed += 1;
-                }
-
-                // if (teamMap.ContainsKey(values[0]) & teamMap.ContainsKey(values[2]))
-                // {
-                //     var hometeamGoals = Int16.Parse(values[1]);
-                //     var outteamGoals = Int16.Parse(values[3]);
-
-                //     // Win/Loss/Draw calculation
-
-                //     if (hometeamGoals > outteamGoals)
-                //     {
-                //         teamMap[values[0]].Wins += 1;
-                //         teamMap[values[2]].Losses += 1;
-                //     }
-                //     else if (hometeamGoals < outteamGoals)
-                //     {
-                //         teamMap[values[0]].Losses += 1;
-                //         teamMap[values[2]].Wins += 1;
-                //     }
-                //     else
-                //     {
-                //         teamMap[values[0]].Draws += 1;
-                //         teamMap[values[2]].Draws += 1;
-                //     }
-
-                //     // Goal for & goal against
-                //     //teamMap[values[0]].UpdateMatchResult(teamMap[values[3]],hometeamGoals,outteamGoals);
-                //     //teamMap[values[1]].UpdateMatchResult();
-
-                //     teamMap[values[0]].GoalsFor += hometeamGoals;
-                //     teamMap[values[2]].GoalsAgainst += outteamGoals;
-                //     teamMap[values[1]].GoalsFor += outteamGoals;
-                //     teamMap[values[3]].GoalsAgainst += hometeamGoals;
-                // }
-
-                // var line = reader.ReadLine();
-                //
-                // Console.WriteLine(line);
-                // var values = line.Split(',');
-                //
-                // Team home =  dictTeams[values[0]];
-                // Team away =  dictTeams[values[2]];
-                // int homePoints = Int16.Parse(values[1]);
-                // int awayPoints = Int16.Parse(values[3]);
-                //
-                // if (!dictTeams.ContainsKey(values[0]) || !dictTeams.ContainsKey(values[2])) {
-                //     return;
-                // }
-                //
-                // home.UpdateMatchResult(away.Abbreviation, homePoints, awayPoints);
-                // away.UpdateMatchResult(home.Abbreviation, awayPoints, homePoints);
-            }
-        }
-    }
-
-
     public static void ProcessLeague(Dictionary<string, Team> dictTeams, League leage, string filepath)
     {
+        // display initial
+        List<Team> teams = dictTeams.Values.ToList();
+        TableDisplay.PrintCurrentStandings(teams);
+
+
+        // all teams play eachother
         for (int i = 1; i <= 22; i++)
         {
             String completePath = Path.Combine(_rootDir, filepath, $"round-{i}.csv");
@@ -122,8 +21,7 @@ public static class RoundProcessor
             ProcessRound(dictTeams, completePath);
         }
 
-        // create fractions
-        List<Team> teams = dictTeams.Values.ToList();
+        // create fractions - reset opponents
         teams.Sort();
         teams.ForEach(team => team.ResetOpponents());
 
@@ -132,28 +30,29 @@ public static class RoundProcessor
         Dictionary<string, Team> upperFractionDict = upperFraction.ToDictionary(team => team.Abbreviation, team => team);
         Dictionary<string, Team> lowerFractionDict = upperFraction.ToDictionary(team => team.Abbreviation, team => team);
 
-        // for (int i = 23; i <= 32; i++)
-        // {
-        //     String completePath = Path.Combine(_rootDir, filepath, $"round-{i}.csv");
-
-        //     ProcessRound(upperFractionDict, completePath);
-        //     ProcessRound(lowerFractionDict, completePath);
-        // }
-
         // set special rankings
         LeagueOld league = new LeagueOld("lala", 2, 1, 1, 1, 2);
-        upperFraction.ForEach(team =>
+        teams.ForEach(team =>
             team.SpecialRanking =
                 (team.SpecialRanking == "R" || team.SpecialRanking == "P") ? "" : team.SpecialRanking);
 
-        lowerFraction.ForEach(team =>
-            team.SpecialRanking =
-                (team.SpecialRanking == "R" || team.SpecialRanking == "P") ? "" : team.SpecialRanking);
-
+        // get promotions
         int totalPromotions = league.GetLeaguePromotions().Sum(position => position.Number);
         int totalRelegations = league.GetLeagueRelegations().Sum(position => position.Number);
 
+        // display initial fractions
+        TableDisplay.PrintCurrentStandings(upperFraction, lowerFraction, null);
 
+        // each fraction play each other
+        for (int i = 23; i <= 32; i++)
+        {
+            String completePath = Path.Combine(_rootDir, filepath, $"round-{i}.csv");
+
+            ProcessRound(upperFractionDict, completePath);
+            ProcessRound(lowerFractionDict, completePath);
+        }
+
+        // display final standings
         TableDisplay.PrintCurrentStandings(upperFraction, lowerFraction, league);
     }
 
@@ -171,30 +70,25 @@ public static class RoundProcessor
 
                 var values = line.Split(',');
 
-                Team home = dictTeams[values[0]];
-                Team away = dictTeams[values[2]];
-
-                int homePoints = int.Parse(values[1]);
-                int awayPoints = int.Parse(values[3]);
-
                 // discard invalid team match-up
                 if (!dictTeams.ContainsKey(values[0]) || !dictTeams.ContainsKey(values[2]))
                 {
                     continue;
                 }
 
+                // if (teamMap.ContainsKey(values[0]) && teamMap.ContainsKey(values[2]))
+                Team home = dictTeams[values[0]];
+                Team away = dictTeams[values[2]];
+
+                int homePoints = int.Parse(values[1]);
+                int awayPoints = int.Parse(values[3]);
+
+
                 // check for match restrictions
                 if (away.HasPlayed(home.Abbreviation, MatchLocation.Outer) ||
                     home.HasPlayed(away.Abbreviation, MatchLocation.Home) ||
                     away.Abbreviation == home.Abbreviation)
                 {
-                    if (filepath == @"c:\Users\danie\UNRAID_SHARE\RiderProjects\Football_Standings\Football_Standings\test\round-12.csv
-")
-                    {
-                        Console.WriteLine("hello debug");
-                    }
-
-
                     Console.WriteLine("Prohibited: Repeated Matches with same opponent or Self");
                     Console.WriteLine(filepath);
                     break;
